4ca1cc0fa69519dde14f17bab5d56457968f93de
XuanHuy <23110102@student.hcmute.edu.vn>
2026-02-20 16:35:34 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,4 @@
-nohup python3 -m torch.distributed.launch --nproc_per_node=4 training/train.py --detector_path ./training/config/detector/sbi.yaml --no-save_ckpt --no-save_feat --ddp > my_output.log 2>&1 &
+python training/train.py \
+--detector_path ./training/config/detector/gend_effort.yaml  \
+--train_dataset "FaceForensics++" \
+--test_dataset  "Celeb-DF-v2" "UADFV" "DFDCP"
