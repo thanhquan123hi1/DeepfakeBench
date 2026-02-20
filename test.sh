abb60c08ad7214644014938b3148127a376b4b22
XuanHuy <23110102@student.hcmute.edu.vn>
2026-02-20 16:35:54 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-python training/train.py \
+python training/test.py \
 --detector_path ./training/config/detector/gend_effort.yaml  \
---train_dataset "FaceForensics++" \
---test_dataset  "Celeb-DF-v2" "UADFV" "DFDCP"
+--test_dataset  "Celeb-DF-v2" "UADFV" "DFDCP" \
+--weights_path /kaggle/input/datasets/xuanhuydinh/deepfakebench/Weight/model_gend.pth \
+--save_feat \
+--feat_out_dir /kaggle/tmp/tsne_pkls
