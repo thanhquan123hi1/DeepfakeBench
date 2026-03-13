1e591f049ba752e7e9a17ea2d8e290e2653b0c8f
XuanHuy <23110102@student.hcmute.edu.vn>
2026-03-14 00:22:14 +0700

    Update test.sh

--- training/test.sh
+++ training/test.sh
@@ -1,6 +1,5 @@
 python training/test.py \
 --detector_path ./training/config/detector/gend_effort.yaml  \
---test_dataset  "Celeb-DF-v2" "UADFV" "DFDCP" \
---weights_path /kaggle/input/datasets/xuanhuydinh/deepfakebench/Weight/model_gend.pth \
---save_feat \
---feat_out_dir /kaggle/tmp/tsne_pkls
+--test_dataset  "Celeb-DF-v2" "UADFV" "DFDCP" "DeepFakeDetection" "DFDC" "uniface_ff" "blendface_ff" "e4s_ff" "facedancer_ff" "fsgan_ff" "inswap_ff" "simswap_ff" \
+--weights_path "/kaggle/temp/logs'/gend_effort_2026-03-13-15-00-18/test/Celeb-DF-v2/ckpt_best.pth"
+
