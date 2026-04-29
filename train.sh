9179caa7e3af7dd12a658d6d8719cb2721feecdc
Thanh Quân <phunglethanhquan1232005@gmail.com>
2026-04-29 17:04:16 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python training/train.py \
 --detector_path ./training/config/detector/gend_effort.yaml  \
 --train_dataset "FaceForensics++" \
---test_dataset  "Celeb-DF-v2" "UADFV" "DFDCP"
+--test_dataset  "Celeb-DF-v2"
